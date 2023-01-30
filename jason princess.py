9fd72fbe39c3e44b02c35a193fb1b752598128a8
Khaohom5 <112546112+Khaohom5@users.noreply.github.com>
2023-01-30 09:20:45 +0700

    Add files via upload

---
+++ jason princess.py
@@ -0,0 +1,13 @@
+'''
+total= 100
+for i in range(10):
+    answer = int(input("Enter a number"))
+    total = total-answer
+print(total)
+    '''
+
+total = 10
+for i in range(10)
+    answer = int(input("Enter a number"))
+    total = total+answer
+print(total)
